1002bb4551280e548210cf4b3e54753c0f6bbd76
Alex Martin <alex@subtleco.co>
2021-05-28 14:50:24 -0500

    completed even or odd

---
+++ even_or_odd.py
@@ -0,0 +1,20 @@
+"""
+
+Create a function that takes one parameter, an integer.
+The function should return "Even" if the parameter is an
+even number or "Odd" if it is not
+
+"""
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    return "Odd"
+
+def test_even_or_odd_solution():
+    assert even_or_odd(2) == 'Even'
+    assert even_or_odd(3) == 'Odd'
+    assert even_or_odd(234) == 'Even'
+    assert even_or_odd(23333) == 'Odd'
+    assert even_or_odd(567) == "Odd"
+    assert even_or_odd(2345499) == "Odd"
